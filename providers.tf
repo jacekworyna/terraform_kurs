93a800eb5682797fd40c5574fdcf8e15d95ce7fa
jacekworyna <jacewor328@student.polsl.pl>
2026-04-29 17:28:52 +0200

    Initial Terraform setup

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
     }
   }
 
-    backend "azurerm" {
+  backend "azurerm" {
     resource_group_name  = "rg-crc2026-student-212-lab"
     storage_account_name = "jworynasatf"
     container_name       = "terraform"
