3d272819e6fb45b7406d00514de1d115fe1fc87d
jacekworyna <jacewor328@student.polsl.pl>
2026-04-29 18:03:05 +0200

    Terraform init changed

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "user_object_id" {
-	description = "(Opcjonalne) Object ID (GUID) Twojego użytkownika w Azure AD. Zostaw puste string ('') aby nie tworzyć polityki dla użytkownika."
-	type        = string
-	default     = ""
+  description = "(Opcjonalne) Object ID (GUID) Twojego użytkownika w Azure AD. Zostaw puste string ('') aby nie tworzyć polityki dla użytkownika."
+  type        = string
+  default     = ""
 }
 
 variable "key_vault_name" {
-	description = "Nazwa Key Vault (jeśli chcesz zmienić domyślną)"
-	type        = string
-	default     = "kvnameofkvhere"
+  description = "Nazwa Key Vault (jeśli chcesz zmienić domyślną)"
+  type        = string
+  default     = "kvnameofkvhere"
 }
